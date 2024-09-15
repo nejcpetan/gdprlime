758fcefee91cce92433cc4bf17e3aa799fbb945b
Nathan Petain <nejc.petan14@gmail.com>
2024-09-15 19:33:11 +0200

    Fixing deployment issues with github pages

--- app/layout.js
+++ app/layout.js
@@ -25,6 +25,9 @@ export default function RootLayout({ children }) {
   }, []);
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/gdprlime/manifest.webmanifest" />
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
